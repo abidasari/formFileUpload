f9bbbc27b391844ad6a5a3c674c379b22e0714a9
abidasari <adasari@usc.edu>
2016-06-13 11:33:51 -0700

    Stable. Flow still unclear but secure and validated. Can still continue by clicking Done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-var upload = false;
-var api = false;
-var OK = false;
-
 var _validFileExtensions = [".pdf"];
 function ValidateSingleInput(oInput) {
     var sFileName = oInput;
@@ -30,83 +26,87 @@ $(document).ready(function (){
   var OK = false;
 
   $('form.step-3 .action-button-holder .download').click(function(){
-      console.log("Download onclick: api- " + api + " upload- " + upload);
+      // console.log("Download onclick: api- " + api + " upload- " + upload);
       $("#triggerDialog")[0].click();
-      $("#button-bar-div").css("margin-top", 30);
-      console.log("Download onclick<end>: api- " + api + " upload- " + upload);
+      // $("#button-bar-div").css("margin-top", 30);
+      // console.log("Download onclick<end>: api- " + api + " upload- " + upload);
   });
 
   $("#dialogCloseBtn").on("click", function(){
       api = false;
+      upload = false;
   });
 
   $("#dialogDoneBtn").on("click", function(){
-      if(upload === true){
-          api = true;
-          upload = false;
-      }
-      if(upload === false){
-          api = true;
-      }
+      upload = false;
+      api = true;
       $('form.step-3 .button-bar .btn').removeClass('disabled');
       $("#uploadFileNameDivP").html("<span style='color: #59a2e7'>UtilitiesAPI will securely share your billing info with us</span>");
   });
 
-
   $('form.step-3 .action-button-holder .upload').on("click", function(){
-      console.log("Upload onclick: api- " + api + " upload- " + upload);
+      $('form.step-3 .button-bar .btn').addClass('disabled');
       $("#uploadFileNameDivP").html("");
-      if(upload === true || api === true)
-          $('form.step-3 .button-bar .btn').addClass('disabled');
       $('#upload').click();
-      console.log("Upload onclick<end>: api- " + api + " upload- " + upload);
   });
 
   $('#upload').change(function(){
-      console.log("upload change: api- " + api + " upload- " + upload);
 
-      var fullPath = this.value;
-      var filename = fullPath.replace(/^.*[\\\/]/, '');
-      console.log("adasarisolar", $('#upload').val());
-      if(ValidateSingleInput(filename) && filename!=""){
-          $("#uploadFileNameDivP").html("<span style='color: #59a2e7'>File Selected:  </span>" + filename);
-          upload = true; api = false;
-          $('form.step-3 .button-bar .btn').removeClass('disabled');
-      }
-      else {
-          $("#uploadFileNameDivP").html("<span style='color: #59a2e7'>Please upload .pdf files only</span>");
-          upload = false; api = false;
-      }
-      console.log("upload change<end>: api- " + api + " upload- " + upload);
+    var fullPath = this.value;
+    var filename = fullPath.replace(/^.*[\\\/]/, '');
+    console.log("adasarisolar", $('#upload').val());
+    if(ValidateSingleInput(filename) && filename!=""){
+        $("#uploadFileNameDivP").html("<span style='color: #59a2e7'>File Selected:  </span>" + filename);
+        upload = true;
+        api = false;
+        $('form.step-3 .button-bar .btn').removeClass('disabled');
+    } else if(!ValidateSingleInput(filename) && filename!="") {
+        $("#uploadFileNameDivP").html("<span style='color: #59a2e7'>Please upload .pdf files only</span>");
+        upload = false;
+        api = false;
+    } else {
+        upload = false;
+        api = false;
+    }
   });
 
   $("#button-bar-div").on("click", function(){
-      console.log("ButtonClicked");
+    console.log("ButtonClicked");
+    var disabled = $("#button-bar-div").hasClass("disabled");
+    if(api == true && upload == false && !disabled){
+      // what to upload to the db?
+    } else if (api == false && upload == true && !disabled){
       $("#form-3-submit").click();
+      // what to upload to db
+    } else {
+      $("#uploadFileNameDivP").html("<span style='color: #59a2e7'>We need your billing details to proceed. Please select one of the options above.</span>");
+    }
   });
 
   $(".step-3").on("submit", function(ev){
-      console.log("File Submitted!");
-      var formData = new FormData();
-      var file = $("#upload").prop('files')[0];
-      formData.append("FILE", file);
-      formData.append("USERID", "USERIDPLACEHOLDER");
+    // console.log("upload: " + upload + "   api: " + api);
+    // console.log("File Submitted!");
+    var formData = new FormData();
+    var file = $("#upload").prop('files')[0];
+    formData.append("FILE", file);
+    formData.append("USERID", "USERIDPLACEHOLDER");
 
-      $.ajax({
-        url: "uploads.php", // Where to send
-        type: "POST", // GET or POST
-        dataType: 'text', // What to expect in return from the server
-        data: formData,
-        success: function(data, textStatus, xhr){
-          console.log("SUCCESS: " + data);
-        },
-        error: function(xhr, textStatus, errThrown){
-          console.log("ERROR: " + textStatus);
-        },
-        processData: false,
-        contentType: false
-      });
+    $.ajax({
+      url: "uploads.php", // Where to send
+      type: "POST", // GET or POST
+      dataType: 'text', // What to expect in return from the server
+      data: formData,
+      success: function(data, textStatus, xhr){
+        // console.log("SUCCESS: " + data);
+        var pathUploadedTo = toString(data);
+      },
+      error: function(xhr, textStatus, errThrown){
+        console.log("ERROR: " + textStatus);
+      },
+      processData: false,
+      contentType: false
+    });
 
-      return false;
+    return false;
   });
 });
--- uploads.php
+++ uploads.php
@@ -1,16 +1,38 @@
 <?php
-  // if(isset($_POST)){
-  //   // print_r("OUT: " . $_POST['test']);
-  //   echo "1: ".$_POST['test']."\n";
-  //   if(isset($_FILES['upload']))
-  //     echo "2: \n";
-  // }
 
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["FILE"]["name"]);
+$uploadOk = 1;
+$pdfFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
+// Check if pdf file is a actual pdf or fake pdf
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["FILE"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($pdfFileType != "pdf" ) {
+    echo "Sorry, only PDF files are allowed.";
+    $uploadOk = 0;
+}
+
+if($uploadOk == 0){
+  echo "Sorry your file was not uploaded. Please try again.";
+} else {
   if ( 0 < $_FILES['FILE']['error'] ) {
-        echo 'Error: ' . $_FILES['FILE']['error'] . '<br>';
-    }
-    else {
-        move_uploaded_file($_FILES['FILE']['tmp_name'], 'uploads/' . $_FILES['FILE']['name']);
-    }
-
+    echo 'Error: ' . $_FILES['FILE']['error'] . '<br>';
+  }
+  else {
+    move_uploaded_file($_FILES['FILE']['tmp_name'], 'uploads/' . $_POST['USERID'] . '.pdf');
+  }
+}
 ?>
